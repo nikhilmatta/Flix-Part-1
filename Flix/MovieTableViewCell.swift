31d2ebd97a09ac3376192de6ef85bac965fbc82a
nikhilmatta <38347071+nikhilmatta@users.noreply.github.com>
2021-02-12 22:55:56 +0530

    Completed assignment 1

---
+++ Flix/MovieTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieTableViewCell.swift
+//  Flix
+//
+//  Created by Nikhil Matta on 12/02/21.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
